6278d135cc6d270febdaa42ee326c29874c01a5d
angganyobaIT <anggaradennise7@gmail.com>
2026-06-02 12:07:42 +0700

    update url

--- API/Authentication/src/docs/swagger.js
+++ API/Authentication/src/docs/swagger.js
@@ -25,7 +25,7 @@ const options = {
         servers: [
             {
                 url:
-                    "http://localhost:3000",
+                    "https://api-paaxpbm.onrender.com",
             },
         ],
 
