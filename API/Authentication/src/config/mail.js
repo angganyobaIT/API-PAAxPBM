b46f0db169efae0b07db3daf08668f4582d9b2ea
angganyobaIT <anggaradennise7@gmail.com>
2026-06-10 03:05:21 +0700

    hhhh fix bug otp

--- API/Authentication/src/config/mail.js
+++ API/Authentication/src/config/mail.js
@@ -1,33 +1,22 @@
-const dns =
-    require("dns");
-
-dns.setDefaultResultOrder(
-    "ipv4first"
-);
-
-
 const nodemailer =
     require("nodemailer");
 
 const transporter =
     nodemailer.createTransport({
 
-        host: "smtp.gmail.com",
+        host: "64.233.170.109",
 
         port: 587,
 
         secure: false,
 
         auth: {
-
             user:
                 process.env.EMAIL_USER,
 
             pass:
                 process.env.EMAIL_PASS,
-        },
-
-        family: 4
+        }
     });
 
 module.exports =
